c58b36c4f49facfc9d51926fc7496e144cd27214
jerrynkwocha <jerrynkwocha@gmail.com>
2026-04-10 00:39:28 +0100

    Corporate UI polish with animations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 function showMessage() {
   alert("You just ran JavaScript!");
 }
+
+// Fade-in on scroll
+const elements = document.querySelectorAll(".fade-in");
+
+window.addEventListener("scroll", () => {
+  elements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (position < screenHeight - 100) {
+      el.classList.add("show");
+    }
+  });
+});
